725216dd7f3ac63c0b9da8360c85f3b309fc1a6c
MartinEBravo <martin.bravo@ug.uchile.cl>
2024-02-07 12:05:27 -0300

    add docs

--- services/get_tokens.py
+++ services/get_tokens.py
@@ -1,5 +1,15 @@
 import tiktoken
 
 def num_tokens_from_string(string: str, model: object) -> int:
+    """
+    Calculates the number of tokens in a given string using the specified model.
+
+    Args:
+        string (str): The input string.
+        model (object): The model object.
+
+    Returns:
+        int: The number of tokens in the string.
+    """
     encoding = tiktoken.encoding_for_model(model.name)
     return len(encoding.encode(string))
--- services/models.py
+++ services/models.py
@@ -1,10 +1,26 @@
 class GPT3:
+	"""
+	Represents the GPT-3.5 Turbo model.
+
+	Attributes:
+		name (str): The name of the model.
+		input_per_token (float): The input cost per token.
+		output_per_token (float): The output cost per token.
+	"""
 	def __init__(self):
 		self.name = "gpt-3.5-turbo"
 		self.input_per_token = 0.0005/1000
 		self.output_per_token = 0.002/1000
 
 class GPT4:
+	"""
+	Represents the GPT-4.0125 Preview model.
+
+	Attributes:
+		name (str): The name of the model.
+		input_per_token (float): The input cost per token.
+		output_per_token (float): The output cost per token.
+	"""
 	def __init__(self):
 		self.name = "gpt-4-0125-preview"
 		self.input_per_token = 0.01/1000
